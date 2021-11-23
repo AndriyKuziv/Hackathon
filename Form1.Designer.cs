0ad5af1dda62a5b516997ce5240906422c7644af
AndriyKuziv <happyandreas18@gmail.com>
2021-11-23 19:15:09 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Testing_see_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(234)))));
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(324, 194);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(526, 277);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.btn_add.Cursor = System.Windows.Forms.Cursors.UpArrow;
+            this.btn_add.FlatAppearance.BorderSize = 0;
+            this.btn_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.btn_add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.btn_add.Location = new System.Drawing.Point(0, 194);
+            this.btn_add.MaximumSize = new System.Drawing.Size(251, 92);
+            this.btn_add.MinimumSize = new System.Drawing.Size(251, 70);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(251, 92);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(234)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.textBox1.Location = new System.Drawing.Point(324, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(234)))));
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(324, 146);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.button1.Location = new System.Drawing.Point(0, 292);
+            this.button1.MaximumSize = new System.Drawing.Size(251, 92);
+            this.button1.MinimumSize = new System.Drawing.Size(251, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 92);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.button2.Location = new System.Drawing.Point(0, 390);
+            this.button2.MaximumSize = new System.Drawing.Size(251, 92);
+            this.button2.MinimumSize = new System.Drawing.Size(251, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 92);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete all";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.label1.Location = new System.Drawing.Point(21, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "oops";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.label2.Location = new System.Drawing.Point(79, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "oops";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(234)))));
+            this.panel1.Controls.Add(this.btn_add);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 1080);
+            this.panel1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.label3.Location = new System.Drawing.Point(319, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Plans for today :";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.label4.Location = new System.Drawing.Point(319, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(290, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Duration of the task (min) :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(234)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(204)))), ((int)(((byte)(191)))));
+            this.textBox2.Location = new System.Drawing.Point(324, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(404, 26);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.ClientSize = new System.Drawing.Size(935, 538);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.MaximumSize = new System.Drawing.Size(951, 577);
+            this.MinimumSize = new System.Drawing.Size(951, 577);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To-Do List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_see_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime thisDay = DateTime.Today;
+
+            label1.Text = "Hello, " + Environment.UserName+"!"+" How's it going?";
+            label2.Text = thisDay.ToString("d");
+
+            if (File.Exists("list.txt")){
+                string line;
+                using (StreamReader sr = new StreamReader("list.txt")){
+                    line = sr.ReadLine();
+                    while (line != null){
+                        checkedListBox1.Items.Add(line, false);
+                        line = sr.ReadLine();
+                    }
+                    sr.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int hours, minutes;     
+
+            if (textBox1.Text != "")
+            { 
+            if (textBox2.Text != "")
+            {
+                    if (Convert.ToInt32(textBox2.Text) > 60)
+                    {
+                        hours = (Convert.ToInt32(textBox2.Text) / 60);
+                        minutes = (Convert.ToInt32(textBox2.Text) % 60);
+                        checkedListBox1.Items.Add(textBox1.Text + " (" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")" + " Duration : " + hours + " hours " + minutes + " minutes", false);
+                    }
+                    else
+                    {
+
+                        checkedListBox1.Items.Add(textBox1.Text + " (" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")" + " Duration : " + textBox2.Text + " minutes", false);
+                    }
+            }
+            else
+                {
+                    checkedListBox1.Items.Add(textBox1.Text + " (" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")", false);
+                }
+            }
+            
+            if (!File.Exists("list.txt")) File.Create("list.txt").Close();
+            using (StreamWriter sw = File.AppendText("list.txt")) {
+                if (textBox2.Text == "")
+                {
+                    sw.WriteLine(textBox1.Text + " (" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")");
+                    sw.Close();
+                }
+                else
+                {
+                    if (Convert.ToInt32(textBox2.Text) > 60)
+                    {
+                        hours = (Convert.ToInt32(textBox2.Text) / 60);
+                        minutes = (Convert.ToInt32(textBox2.Text) % 60);
+                        sw.WriteLine(textBox1.Text + " (" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")" + " Duration : " + hours + " hours " + minutes + " minutes");
+                    }
+                    else
+                    {
+                        sw.WriteLine(textBox1.Text + "(" + dateTimePicker1.Value.ToString("dd.MM.yyyy") + ")" + " Duration : " + textBox2.Text + " minutes");
+                        sw.Close();
+                    }
+                }
+            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string line;
+            string target = Convert.ToString(checkedListBox1.SelectedItem);
+            if (!File.Exists("buf.txt")) File.Create("buf.txt").Close();
+            using (StreamReader sr = new StreamReader("list.txt")){
+                line = sr.ReadLine();
+                while (line != null){
+                    if (line != target) using (StreamWriter sw2 = File.AppendText("buf.txt")) sw2.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+
+            File.Delete("list.txt");
+            File.Create("list.txt").Close();
+
+            using (StreamReader sr2 = new StreamReader("buf.txt")){
+                line = sr2.ReadLine();
+                while (line != null){
+                    using (StreamWriter sw = File.AppendText("list.txt")) { 
+                        sw.WriteLine(line);
+                        sw.Close();
+                    }
+                    line = sr2 .ReadLine();
+                }
+            }
+            File.Delete("buf.txt");
+
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+
+            //using (StreamReader fr = new StreamReader("list.txt")){
+            //    line = fr.ReadLine();
+            //    while (true){
+            //        try
+            //        {
+            //            if (line == null) File.Delete("list.txt");
+            //            break;
+            //        }
+            //        catch
+            //        {
+            //            break;
+            //            //Thread.Sleep(1000);
+            //        }
+            //    }
+
+
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            if (File.Exists("list.txt")) File.Delete("list.txt");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
